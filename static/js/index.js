21660bd3e96a91db0f7f7c751c0b934bef6325cd
Jack Grodnick <jackgrodnick@jacks-mbp.lan>
2020-06-01 14:23:30 -0500

    cards

--- static/js/index.js
+++ static/js/index.js
@@ -240,6 +240,7 @@ function randomColor() {
 let blackjackGame = {
   'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
   'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+  'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
 }
 
 const YOU = blackjackGame.you
@@ -249,19 +250,32 @@ const hitSound = new Audio('./static/css/sounds/swish.m4a');
 
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+
 function blackjackHit() {
   showCard(YOU);
 }
 
-showCard = (activePlayer) => {
+
+
+function showCard(activePlayer) {
   let cardImage = document.createElement('img');
   cardImage.src = "./static/css/images/Q.png";
   document.querySelector(activePlayer.div).appendChild(cardImage);
   hitSound.play();
-}
+};
 
-blackjackDeal = () => {
-  let yourImages = document.querySelector('#your-box').querySelectorall('img')
+function blackjackDeal() {
+  let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+  let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+
+  for (let i = 0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+  }
+  for (let i = 0; i < dealerImages.length; i++) {
+    dealerImages[i].remove();
+  }
 }
 
 
