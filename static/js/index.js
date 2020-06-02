1e108c93cf65222cb52529aecc2a9cb10fef20e4
Jack Grodnick <jackgrodnick@jacks-mbp.lan>
2020-06-01 22:01:44 -0500

    blackjackbot

--- static/js/index.js
+++ static/js/index.js
@@ -241,17 +241,23 @@ let blackjackGame = {
   'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
   'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
   'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
-  'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11]}
+  'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11]},
+  'wins': 0,
+  'losses': 0,
+  'draws': 0,
 }
 
 const YOU = blackjackGame.you
 const DEALER = blackjackGame.dealer
 
 const hitSound = new Audio('./static/css/sounds/swish.m4a');
+const winSound = new Audio('./static/css/sounds/cash.mp3')
+const lossSound = new Audio('./static/css/sounds/aww.mp3')
 
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 
 
 function blackjackHit() {
@@ -292,8 +298,10 @@ function blackjackDeal() {
   document.querySelector(YOU.scoreSpan).textContent = '0';
   document.querySelector(YOU.scoreSpan).style.color = 'white'
   document.querySelector(DEALER.scoreSpan).textContent = '0';
-  document.querySelector(DEALER.scoreSpan).style.color = 'white'
+  document.querySelector(DEALER.scoreSpan).style.color = 'white';
 
+  document.querySelector('#blackjack-result').textContent = "Let's play";
+  document.querySelector('#blackjack-result').style.color = "black";
 }
 
 function updateScore(card, activePlayer) {
@@ -308,17 +316,6 @@ function updateScore(card, activePlayer) {
 } else {
   activePlayer.score += blackjackGame.cardsMap[card];
 }
-
-
-
-
-
-
-
-
-
-
-
 
   /* if (blackjackGame.cardsMap[card][1] === 11 || blackjackGame.cardsMap[card][0] === 1 && activePlayer.score <= 10) {
     let card0 = 11;
@@ -342,7 +339,72 @@ function showScore(activePlayer) {
  }
 }
 
+function dealerLogic() {
+  let card = randomCard();
+  showCard(card, DEALER);
+  updateScore(card, DEALER)
+  showScore(DEALER);
+  if (DEALER.score > 15) {
+    let winner = computeWinner();
+    showResult(winner)
+  } else if (DEALER.score <= 15) {
+    dealerLogic()
+  }
+}
 
+// compute winner and return who just won
+// update the wins, draws, and losses
+function computeWinner() {
+  let winner;
+
+  if (YOU.score <= 21) {
+    // condition: higher score than dealer or when dealer busts but you're 21 or under
+    if (YOU.score > DEALER.score || (DEALER.score > 21)) {
+      blackjackGame.wins++;
+      winner = YOU
+    } else if (YOU.score < DEALER.score) {
+      blackjackGame.losses++;
+      winner = DEALER
+    } else if (YOU.score === DEALER.score) {
+      blackjackGame.draws++;
+    }
+
+    // condition: when user busts but dealer doenst
+  } else if (YOU.score > 21 && DEALER.score <= 21) {
+    blackjackGame.losses++;
+    winner = DEALER;
+
+
+    // condition: when you AND the Dealer busts
+  } else if (YOU.score > 21 && DEALER.score > 21) {
+    blackjackGame.draws++;
+  }
+
+  return winner
+}
+
+function showResult(winner) {
+  let message, messageColor;
+
+  if (winner === YOU) {
+    document.querySelector('#wins').textContent = blackjackGame.wins;
+    message = 'You won!';
+    messageColor = 'green';
+    winSound.play();
+  } else if (winner === DEALER) {
+    document.querySelector('#losses').textContent = blackjackGame.losses;
+    message = 'You lost!';
+    messageColor = 'red';
+    lossSound.play();
+  } else {
+    document.querySelector('#draws').textContent = blackjackGame.draws;
+    message = 'You drew!';
+    messageColor  = 'black';
+  }
+  document.querySelector('#blackjack-result').textContent = message;
+  document.querySelector('#blackjack-result').style.color = messageColor;
+
+}
 
 
 
